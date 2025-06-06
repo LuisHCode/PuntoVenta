f1c1bf430fb3d9ad36fa358cf1a471088797eaba
Luis Lopez <yolothelife95@gmail.com>
2025-06-06 10:23:35 -0600

    se ha cambiado el procedimiento almacenado de auth para poder verificar de buena manera el refrescar, ahora refresca una unica vez y pide el cambio de token

--- db_dump/proc_auth.sql
+++ db_dump/proc_auth.sql
@@ -2,7 +2,7 @@ use taller;
 DELIMITER $$
 CREATE DEFINER=`root`@`localhost` PROCEDURE `verificarTokenR` (IN `_idUsuario` VARCHAR(100), IN `_tkRef` VARCHAR(255))   
 begin
-    select rol from usuario where idUsuario = _idUsuario OR correo = _idUsuario and tkRef = _tkRef;
+    select rol from usuario where (idUsuario = _idUsuario OR correo = _idUsuario) and tkRef = _tkRef;
 end$$
 CREATE DEFINER=`root`@`localhost` FUNCTION `modificarToken` (`_idUsuario` VARCHAR(100), `_tkRef` VARCHAR(255)) 
     RETURNS INT DETERMINISTIC READS SQL DATA begin
